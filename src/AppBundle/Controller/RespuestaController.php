26cba13441395ba046e7f92d9309843ccf81da53
Jonathan Cespedes <cespedesjonathan25@gmail.com>
2018-02-15 17:49:21 -0400

    Respuestas configuradas

---
+++ src/AppBundle/Controller/RespuestaController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Ticket;
+use AppBundle\Entity\Respuesta;
+
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class RespuestaController extends Controller
+{
+  /**
+  * @Route("/admin/respuestas", name="respuestas")
+  */
+  public function respuestasAction(){
+    $em = $this->getDoctrine()->getManager();
+    $list = $this->getDoctrine()->getRepository('AppBundle:Ticket')->findAll();
+
+    $query = $em->createQuery('SELECT
+      u.username, t.id, t.titulo, t.estado, t.fecha, t.categoria, t.prioridad
+      FROM AppBundle:User u
+      INNER JOIN AppBundle:Ticket t
+      WITH u.id = t.idUsuario
+      WHERE t.estado = :estado')->setParameter('estado', 'Procesando');
+
+    $ticket = $query->getResult();
+
+    return $this->render('respuesta/respuesta.html.twig', array('ticket' => $ticket));
+  }
+
+  /**
+  * @Route("/admin/respuestas/{id}", name="respondiendo")
+  */
+
+  public function respAction($id, Request $request){
+
+    $respuesta = new Respuesta();
+
+    $ticket = $this->getDoctrine()
+    ->getRepository('AppBundle:Ticket')
+    ->find($id);
+
+    $form = $this->CreateFormBuilder()
+    ->add('titulo', TextType::class, array('attr'=>array('class'=>'form-control', 'style' => 'margin-bottom:15px')))
+    ->add('respuesta', TextareaType::class, array('attr'=>array('class'=>'form-control', 'style' => 'margin-bottom:15px')))
+    ->add('categoria', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+      'choices'  => array(
+      'Tecnologia' => 'tech',
+      'Administrativo' => 'admin',
+      'Servicio' => 'serv'),))
+    ->add('prioridad', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+      'choices'  => array(
+      'Alto' => 'Alto',
+      'Normal' => 'Normal',
+      'Bajo' => 'Bajo'),))
+    ->add('estado', ChoiceType::class, array('attr'=>array('class'=>'nav-link dropdown-toggle', 'style' => 'margin:10px'),
+      'choices'  => array(
+      'Procesando' => 'Procesando',
+      'Declinado' => 'Declinado',
+      'Resuelto' => 'Resuelto'),))
+    ->add('Enviar Respuesta', SubmitType::class, array('attr'=>array('class'=>'btn btn-primary', 'style' => 'margin:15px')))
+    ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $title = $form['titulo']->getData();
+      $descripcion = $form['respuesta']->getData();
+      $testado = $form['estado']->getData();
+      $tprioridad = $form['prioridad']->getData();
+      $tcategoria = $form['categoria']->getData();
+
+      $respuesta->setTitulo($title);
+      $respuesta->setRespuesta($descripcion);
+      $respuesta->setTicketId($id);
+
+      $ticket->setEstado($testado);
+      $ticket->setCategoria($tcategoria);
+      $ticket->setPrioridad($tprioridad);
+
+      $em = $this->getDoctrine()->getManager();
+
+      $em->persist($respuesta);
+      $em->flush();
+
+      $this->addFlash(
+        'notice', 'respuesta enviada'
+      );
+
+      return $this->redirectToRoute('respuestas');
+
+  }
+
+  return $this->render('respuesta/respondiendo.html.twig', array(
+    'respuesta' => $respuesta,
+    'ticket' => $ticket,
+    'form' => $form->createView(),
+  ));
+  }
+}
---
+++ src/AppBundle/Entity/Respuesta.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Respuesta
+ *
+ * @ORM\Table(name="respuesta")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RespuestaRepository")
+ */
+class Respuesta
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ticket_id", type="integer")
+     */
+    private $ticketId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="respuesta", type="text")
+     */
+    private $respuesta;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ticketId
+     *
+     * @param integer $ticketId
+     *
+     * @return Respuesta
+     */
+    public function setTicketId($ticketId)
+    {
+        $this->ticketId = $ticketId;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketId
+     *
+     * @return int
+     */
+    public function getTicketId()
+    {
+        return $this->ticketId;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     *
+     * @return Respuesta
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set respuesta
+     *
+     * @param string $respuesta
+     *
+     * @return Respuesta
+     */
+    public function setRespuesta($respuesta)
+    {
+        $this->respuesta = $respuesta;
+
+        return $this;
+    }
+
+    /**
+     * Get respuesta
+     *
+     * @return string
+     */
+    public function getRespuesta()
+    {
+        return $this->respuesta;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return Respuesta
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+}
+
